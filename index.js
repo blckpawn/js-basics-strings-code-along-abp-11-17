c342891e53dff803c620d9f1960aab938ef656bf
Brian Winter <blackpawn8990@gmail.com>
2017-11-08 02:19:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = "Joe Dirt";
 
-const welcomeMessage = 'Welcome to Flatbook, ' +  ${currentUser} '!';
+const welcomeMessage = 'Welcome to Flatbook, ' +  currentUser '!';
 
